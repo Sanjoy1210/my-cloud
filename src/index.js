f9ef9014047a9e41bb2d7c8a0e992d51ac2a135d
Sanjoy1210 <sanjoypp@gmail.com>
2023-08-25 15:08:21 +0600

    responsive complete

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import "./fonts/sofia-pro/fonts.scss";
 // dropdown menu
 const user = document.getElementById("user-info");
 const dropdown = document.getElementById("user-dropdown");
+const body = document.body;
 
 function handleToggleDropdown() {
   if (dropdown.classList.contains('active')) {
@@ -21,10 +22,12 @@ user.addEventListener("click", handleToggleDropdown);
 // drawer toggle
 window.addEventListener("click", event => {
   
-  if(event.target.className == "drawer-btn" || event.target.className == "toggle") {
+  if(event.target.className == "drawer-bnt" || event.target.className == "toggle") {
     document.body.classList.toggle("show");
+    body.style.position = "fixed";
   } else if (event.target.className == "overlay") {
     document.body.classList.remove("show");
+    body.style.position = "";
   }
 });
 
